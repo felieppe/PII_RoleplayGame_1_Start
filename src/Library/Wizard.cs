f91fff315729fe7fcb25305fef1b4aba61cc8e0e
Felipe <me@felieppe.com>
2023-09-11 10:46:40 -0300

    Added again Name param

--- src/Library/Wizard.cs
+++ src/Library/Wizard.cs
@@ -3,9 +3,12 @@ using System.Collections;
 namespace Program;
 
 public class Wizard {
+    private string Name;
     private float Health = 100;
     private ArrayList Items;
-    public Wizard() {}
+    public Wizard(string name) {
+        this.Name = name;
+    }
 
     public void AddItem(Item item) {
         Items.Add(item);
