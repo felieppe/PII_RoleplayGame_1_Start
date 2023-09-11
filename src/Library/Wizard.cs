ef172f07a079423b2a73946de0ba5756ac277bb9
Felipe <me@felieppe.com>
2023-09-11 10:13:03 -0300

    Wizard class created

---
+++ src/Library/Wizard.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Program;
+
+public class Wizard {
+    private string Name;
+    private float Health = 100;
+    private ArrayList Items;
+    public Wizard() {}
+}
