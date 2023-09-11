d3178d2f357273abe985b4d7949cf16bde165b1e
Stefano <stefano.francolino@correo.ucu.edu.uy>
2023-09-11 10:44:40 -0300

    Ejercicio

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,6 +7,8 @@ namespace Program
         static void Main(string[] args)
         {
             Item MagicWand = new Item ("Magic Wand","Attachment",2,10,0);
+            /* Wizzard wizzard = new Wizzard ();
+            */
         }
     }
 }
