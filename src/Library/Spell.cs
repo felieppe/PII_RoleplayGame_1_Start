810f43785449f200b631402d40634ccbe0af8890
Felipe <me@felieppe.com>
2023-09-11 11:44:26 -0300

    Name param added

--- src/Library/Spell.cs
+++ src/Library/Spell.cs
@@ -1,5 +1,19 @@
 namespace Program;
 
 public class Spell {
-    public Spell() {}
+    private string Name;
+
+    public Spell(string name) {
+        this.Name = name;
+    }
+
+    public string GetName() {
+        return this.Name;
+    }
+
+    public void SetName(string name) {
+        if (string.IsNullOrEmpty(name)) {
+            this.Name = name;
+        }
+    }
 }
