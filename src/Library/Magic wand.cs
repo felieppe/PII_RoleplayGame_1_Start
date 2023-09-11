b30535a47f28ce705c28ab7aa98de51893204ac1
Stefano <stefano.francolino@correo.ucu.edu.uy>
2023-09-11 10:27:44 -0300

    Magic

---
+++ src/Library/Magic wand.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Dynamic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Program;
+
+public class Magicwand{
+    
+    private int damage = 10;
+    private int defense = 0; 
+public int GetDefensePoints(){
+    return this.defense;
+    }
+}
+
