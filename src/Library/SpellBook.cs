67496c97289fb56248d443ebfd2574ff6c34cbb1
Lucia Vazquez <luciavazquezreyes299@gmail.com>
2023-09-11 10:23:43 -0300

    Clase SpellBook agregada

---
+++ src/Library/SpellBook.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Program;
+
+public class SpellBook{
+    private ArrayList Spells = new ArrayList();
+
+    public ArrayList GetSpells(){
+        return this.Spells;
+
+    }
+    }
+
+
