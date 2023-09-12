0337d7d0d06aa72e09a07bbbf41d3e54600d50ec
Felipe <me@felieppe.com>
2023-09-12 10:13:19 -0300

    SetType() func added

--- src/Library/CharacterType.cs
+++ src/Library/CharacterType.cs
@@ -1,7 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+
 namespace Program;
 
 public class CharacterType {
-    public CharacterType() {
-        
+    private List<string> AvailableTypes = new List<string>() {"Wizard", "Elve", "Dwarve"};
+    private string ActualType;
+
+    public CharacterType() {}
+
+    public bool SetType(string ty) {
+        if (AvailableTypes.Contains(ty)) {
+            this.ActualType = ty;
+            return true;
+        } else { return false; }
     }
 }
