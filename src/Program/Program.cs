0837b1398e73a22e0e5016810400782e598f093f
Felipe <me@felieppe.com>
2023-09-13 09:20:09 -0300

    Commented all the code of program.cs

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,13 +6,15 @@ namespace Program
     {
         static void Main(string[] args)
         {
+            /*ICharacter character = new Wizard();
+
             Item magicWand = new Item ("Magic Wand","Attachment",2,10,0);
             SpellBook sbook = new SpellBook();
 
             CharacterType type = new CharacterType();
             type.SetType("Wizard");
             Character wiz = new Character("Alfred The Wizard", type);
-            wiz.AddItem(magicWand);
+            wiz.AddItem(magicWand);*/
         }
     }
 }
