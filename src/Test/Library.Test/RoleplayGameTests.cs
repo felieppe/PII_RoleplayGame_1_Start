a254d3c59abcefed4d9c47c51de42df27afdf7c8
felieppe <me@felieppe.com>
2023-09-17 17:16:55 -0300

    Explation for TestBlankWizardName() test case added

--- src/Test/Library.Test/RoleplayGameTests.cs
+++ src/Test/Library.Test/RoleplayGameTests.cs
@@ -4,6 +4,13 @@ namespace Program
 {
     public class RoleplayGameTests
     {
+        /*
+            JUSTIFICACIÓN:
+            Este caso de prueba lo que revisa es si, al agregar un aspecto tan importante
+            como el nombre de un personaje, es vacio o nulo.
+            Si el llamado de la función devuelve TRUE, significa que el nombre pasó las validaciones correspondientes.
+            Si devuelve FALSE, es sinonimo que el nombre no es valido para el personaje. 
+        */
         [Test]
         public void TestBlankWizardName() {
             Wizard wiz = new Wizard();
